0dfd281369d6b89960a4a7b4b62d1072ef736e66
Paulina Vega <67831111+paulinavegat@users.noreply.github.com>
2021-06-04 22:38:46 -0400

    actividades en java

---
+++ JAVA/Geometria/src/Triangulo.java
@@ -0,0 +1,15 @@
+
+public class Triangulo {
+	
+	double base;
+	double altura;
+	double area;
+	
+	
+	 void area() {
+		area = (base*altura)/2;
+		System.out.println(area);
+		
+	}
+
+}
---
+++ JAVA/Geometria/src/TrianguloTest.java
@@ -0,0 +1,15 @@
+
+public class TrianguloTest {
+
+	public static void main(String[] args) {
+		
+		Triangulo t1 = new Triangulo();
+	
+		t1.base = 10;
+		t1.altura = 5;
+		//System.out.println(t1.altura);
+		t1.area();
+
+	}
+
+}
