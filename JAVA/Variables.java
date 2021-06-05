fee4a21d2e90d6daba1b8d27c9d95199b9e6b069
Paulina Vega <67831111+paulinavegat@users.noreply.github.com>
2021-06-04 22:50:17 -0400

    variables

---
+++ JAVA/Variables.java
@@ -0,0 +1,13 @@
+public class Variables { 
+    public static void main(String[] args){
+        //Podemos declarar una variable sin valor. 
+        int nuestroInt; 
+        //Podemos asignar un valor a la variable, después en nuestro código. 
+        nuestroInt = 400; 
+        //También podemos declarar y asignar en la misma línea. 
+        double pi = 3.14159265;
+        boolean bool = true; 
+        char unCaracter = 'A'; 
+        String unaCadena = "ABC"; 
+    }
+}
