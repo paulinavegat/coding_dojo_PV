81621d1d05a1cd1dac30cb9a72fa095856b9710d
Paulina Vega <67831111+paulinavegat@users.noreply.github.com>
2021-06-04 22:43:55 -0400

    Delete JAVA/PrimerJava directory

--- JAVA/PrimerJava/src/PrimerJava.java
+++
@@ -1,9 +0,0 @@
-
-public class PrimerJava {
-
-	  public static void main(String[] args) {
-		    System.out.println("Mi nombre es Paulina Vega");
-		    System.out.println("Mi edad es 34");
-		    System.out.println("Mi ciudad natal es Punta Arenas");
-		  }
-		}
