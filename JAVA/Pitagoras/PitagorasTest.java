ebb03e4ec6826edabcbd7eb2b2cd49ce6379abd3
Paulina Vega <67831111+paulinavegat@users.noreply.github.com>
2021-06-13 21:50:52 -0400

    Calcular hipotenusa

---
+++ JAVA/Pitagoras/PitagorasTest.java
@@ -0,0 +1,12 @@
+public class PitagorasTest {
+  public static void main (String[] args){
+    int ladoA = 3;
+    int ladoB = 4;
+    double ladoC;
+    ladoC = Pitagoras.calcularHipotenusa(ladoA, ladoB);
+
+    System.out.println("Hipotenusa es: " + ladoC);
+
+
+  }
+}
