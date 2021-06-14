9c4778985ef38ee3a7acca017846f43e6daef6f3
Paulina Vega <67831111+paulinavegat@users.noreply.github.com>
2021-06-14 12:23:42 -0400

    Delete Pitagoras.java

--- JAVA/Pitagoras.java
+++
@@ -1,8 +0,0 @@
-public class Pitagoras{
-	
-  public static double calcularHipotenusa(int catetoA, int catetoB) {
-    double hipotenusa = 0.0;
-    hipotenusa = Math.sqrt( Math.pow(catetoA, 2) + Math.pow(catetoB, 2) );
-    return hipotenusa;
-  }
-}
