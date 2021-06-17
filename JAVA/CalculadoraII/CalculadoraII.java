3c6075250ba9660cf6339dc49c2cca9add151717
Paulina Vega <67831111+paulinavegat@users.noreply.github.com>
2021-06-17 19:18:45 -0400

    Add files via upload

--- JAVA/CalculadoraII/CalculadoraII.java
+++ JAVA/CalculadoraII/CalculadoraII.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 public class CalculadoraII{
 	//atributos
 	private static double resultado;
-	private static ArrayList<Double> numeros = new ArrayList<Double>();
-	private static ArrayList<String> operaciones = new ArrayList<String>();
+	private  ArrayList<Double> numeros = new ArrayList<Double>();
+	private ArrayList<String> operaciones = new ArrayList<String>();
 
 	//constructor
 	public CalculadoraII() {
@@ -33,32 +33,43 @@ public class CalculadoraII{
 			if (operaciones.get(i) == "*" || operaciones.get(i) == "/") {
 				if (operaciones.get(i) == "*") {
 					resultado = numeros.get(i) * numeros.get(i + 1);
+					 //System.out.println(this.numeros);
+		             //System.out.println(this.operaciones);
 				}
 				else if (operaciones.get(i) == "/") {
 					resultado = numeros.get(i) / numeros.get(i + 1);
+					// System.out.println(this.numeros);
+		            // System.out.println(this.operaciones);				
 				}
 				numeros.set(i + 1, resultado);
 				numeros.remove(i); //remover los numeros que se multiplicaban o dividian, dejando su resultado
-				operaciones.remove(i); // remover la operacion de mult o dividir del conjunto de operaciones, ya que nos quedamos solo con su resultado
+				operaciones.remove(i);// remover la operacion de mult o dividir del conjunto de operaciones, ya que nos quedamos solo con su resultado
+				i--; //disminuyo una posicion ya que dos valores multiplicados o divididos se convierte en uno
 			}
 		}
+		
 		for(int i = 0; i < operaciones.size(); i ++) {
-			i = 0;
-			if (operaciones.get(i) == "+" || operaciones.get(i) == "-") {
-				if (operaciones.get(i) == "+") {
+				if (operaciones.get(i) == "+" || operaciones.get(i) == "-") {
+			    	if (operaciones.get(i) == "+") {
 					resultado = numeros.get(i) + numeros.get(i + 1);
+					//System.out.println(this.numeros);
+		             //System.out.println(this.operaciones);
 				}
 				else if (operaciones.get(i) == "-") {
 					resultado = numeros.get(i) - numeros.get(i + 1);
 				}
+					//System.out.println(this.numeros);
+		            //System.out.println(this.operaciones);
 				numeros.set(i + 1, resultado);
 				numeros.remove(i);
 				operaciones.remove(i);
+				i--;//disminuyo una posicion ya que dos valores sumados o restado se convierte en uno
+				
 			}
 		}
 		
 		if (operaciones.get(0) == "=") {
-			System.out.println("El resultado es: " + numeros);
+			System.out.println("El resultado es: " + resultado);
 		}
 	}
 }
